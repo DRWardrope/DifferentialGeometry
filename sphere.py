dcc4d091a0147754c1a3033bbbb5893175a77164
David Wardrope <DRWardrope@gmail.com>
2019-03-12 16:08:05 +0000

    Defining operations for the spherical case.

--- sphere.py
+++ sphere.py
@@ -1,12 +1,13 @@
 #import numpy as np
 from manifold import Manifold
 from metric import EuclideanMetric
-from numpy import arccos
+from numpy import arccos, cos, finfo, float, sin, sqrt, where
 
 class Sphere(Manifold):
     '''
         (Hyper-)Spherical manifolds. Assumes n-dimensional
-        manifold is embedded in an (n+1)-dimensional ambient space
+        manifold is embedded in an (n+1)-dimensional ambient space.
+        Assume radius of the sphere is 1.
     '''
     def __init__(self, n_dims):
         '''
@@ -24,3 +25,68 @@ class Sphere(Manifold):
         '''
         return arccos(self.metric.dot(u, v))
 
+    def norm(self, u):
+        '''
+        Calculate the norm of u
+        :param u: (m, n_dims) np.array, representing m vectors:
+        :return: (m, 1) dimensional np.array, representing the norm of u
+        '''
+        return sqrt(self.metric.dot(u, u))
+
+    def project_to_tangent_space(self, point, vector):
+        '''
+        Project vector into tangent space of point.
+        Since point is on hypersphere of radius 1, point.point = +1
+        :param point:  (m, n_dims) np.array, representing m points on the
+                        hypersphere:
+        :param vector: (m, n_dims) np.array, representing m vectors in ]
+                        (n+1)-dimensional ambient space
+        :return: (m, n_dims) np.array, representing m vectors projected to
+                tangent spaces of point
+        '''
+        return vector - self.metric.dot(point, vector)*point
+
+    def exponential_map(self, point, v_TpS):
+        '''
+        Follow geodesic in direction v_TpS from point and
+        return the resulting point.
+
+        :param point: (m, n_dims) np.array, representing m points on the
+                        hypersphere:
+        :param v_TpS: (m, n_dims) np.array, representing m vectors projected to
+                tangent spaces of point:
+        :return: (m, n_dims) np.array, m points on the hypersphere along the
+                geodesic chosen by v_TpS, from point
+        '''
+        norm_v_TpS = self.norm(v_TpS)
+        # If v_TpS has zero norm, return the original point.
+        # Correct behaviour and avoids division by zero in following calculation
+        return where(
+                        norm_v_TpS < finfo(float).eps,
+                        point,
+                        cos(norm_v_TpS) * point +
+                                            sin(norm_v_TpS) * (v_TpS/norm_v_TpS)
+                     )
+
+
+    def logarithmic_map(self, point0, point1):
+        '''
+        Inverse of exponential map
+
+        :param point0: (m, n_dims) np.array, representing m "base" points:
+        :param point1: (m, n_dims) np.array, representing m "target" points
+        :return: (m, n_dims) np.array, m vectors in tangent spaces of point0,
+                that would yield point1 if inserted in exponential map
+        '''
+        dot01 = self.metric.dot(point0, point1)
+        v_Tp0M = point1 - dot01*point0
+        dist = self.distance(point0, point1)
+        norm_v_Tp0M = self.norm(v_Tp0M)
+
+        # If v_TpS has zero norm, return the original point.
+        # Correct behaviour and avoids division by zero in following calculation
+        return where(
+                        norm_v_Tp0M < finfo(float).eps,
+                        v_Tp0M,
+                        v_Tp0M*dist/norm_v_Tp0M,
+                     )
--- tests/test_sphere.py
+++ tests/test_sphere.py
@@ -16,8 +16,80 @@ def test_distance():
     expected = np.array([[0.5], [0.], [3.066370614359173]])
 
     circle = Sphere(1)
-    print("")
-    print(u)
-    print(v)
-    print(circle.distance(u, v))
     assert_array_almost_equal(circle.distance(u, v), expected)
+
+def test_project_to_tangent_space():
+    p = np.array([
+                    [np.cos(0.), np.sin(0.)],
+                    [np.cos(0.), np.sin(0.)],
+                    [np.cos(0.), np.sin(0.)],
+                    [np.cos(0.5*np.pi), np.sin(0.5*np.pi)],
+                    [np.cos(np.pi), np.sin(np.pi)]
+                ])
+    v = np.array([
+                    [0., 0.],
+                    [1., 0.],
+                    [1., 1.],
+                    [1., 1.],
+                    [1., 1.]
+    ])
+    expected = np.array([
+                    [0., 0.],
+                    [0., 0.],
+                    [0., 1.],
+                    [1., 0.],
+                    [0., 1.]
+    ])
+    circle = Sphere(1)
+    assert_array_almost_equal(circle.project_to_tangent_space(p, v), expected)
+
+def test_exponential_map():
+    p = np.array([
+                    [np.cos(0.), np.sin(0.)],
+                    [np.cos(0.), np.sin(0.)],
+                    [np.cos(0.5*np.pi), np.sin(0.5*np.pi)],
+                    [np.cos(np.pi), np.sin(np.pi)]
+                ])
+    v = np.array([
+                        [0.,         0.],
+                        [0.,         0.25*np.pi],
+                        [0.25*np.pi, 0.],
+                        [0.,         0.25*np.pi]
+    ])
+    expected = np.array([
+                    [1., 0.],
+                    [0.707106781186548, 0.707106781186548],
+                    [0.707106781186548, 0.707106781186548],
+                    [-0.707106781186548, 0.707106781186548]
+    ])
+    circle = Sphere(1)
+    print("")
+    print(circle.exponential_map(p, v))
+    print(expected)
+    assert_array_almost_equal(circle.exponential_map(p, v), expected)
+
+def test_logarithmic_map():
+    p0 = np.array([
+                    [np.cos(0.), np.sin(0.)],
+                    [np.cos(0.), np.sin(0.)],
+                    [np.cos(0.5*np.pi), np.sin(0.5*np.pi)],
+                    [np.cos(np.pi), np.sin(np.pi)]
+                ])
+    p1 = np.array([
+                    [1., 0.],
+                    [0.707106781186548, 0.707106781186548],
+                    [0.707106781186548, 0.707106781186548],
+                    [-0.707106781186548, 0.707106781186548]
+    ])
+    expected = np.array([
+                        [0.,         0.],
+                        [0.,         0.25*np.pi],
+                        [0.25*np.pi, 0.],
+                        [0.,         0.25*np.pi]
+    ])
+
+    circle = Sphere(1)
+    print("")
+    print(circle.logarithmic_map(p0, p1))
+    print(expected)
+    assert_array_almost_equal(circle.logarithmic_map(p0, p1), expected)
